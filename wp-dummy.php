aa2457153f229e630804ee2ad5215da1e66dda22
mbsouth <84234150+mbsouth@users.noreply.github.com>
2023-07-31 15:20:37 +0200

    Create wp-dummy.php

---
+++ wp-dummy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP Dummy
+ * Description: A dummy plugin exclusively for testing purposes.
+ * Author:      mbSouth
+ *
+ * Version:     1.0.0
+ */
+
+require_once plugin_dir_path(__FILE__) . 'includes/wp-dummy-functions.php';
+
