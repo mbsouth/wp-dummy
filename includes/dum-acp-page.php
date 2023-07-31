522d2fae308d02ea601749b01e4eb60ddd8f4b9c
mbsouth <84234150+mbsouth@users.noreply.github.com>
2023-07-31 15:38:54 +0200

    Update dum-acp-page.php

--- includes/dum-acp-page.php
+++ includes/dum-acp-page.php
@@ -1,1 +1,10 @@
+<?php
+/**
+ * Add my new menu to the Admin Control Panel
+ */
 
+echo '<div class="wrap">'
+    .  '<h1>Hello World!</h1>'
+    .  '<p>Welcome to the super awesome plugin!</p>'
+    .'</div>'
+    ;
