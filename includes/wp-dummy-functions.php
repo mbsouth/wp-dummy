afc7437a63e281c438f54a8d02337e32a77b3780
mbsouth <84234150+mbsouth@users.noreply.github.com>
2023-07-31 15:34:27 +0200

    Update wp-dummy-functions.php

--- includes/wp-dummy-functions.php
+++ includes/wp-dummy-functions.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * Add my new menu to the Admin Control Panel
+ */
+add_action( 'admin_menu', 'dum_add_admin_link' );
 
+// Add a new top level menu link to the ACP
+function dum_add_admin_link()
+{
+      add_menu_page(
+        'WP Dummy',                  // Page title
+        'WP Dummy',                  // Menu link
+        'manage_options',            // Capability requirement
+        'includes/dum-acp-page.php'  // File to display when clicking the link
+    );
+}
